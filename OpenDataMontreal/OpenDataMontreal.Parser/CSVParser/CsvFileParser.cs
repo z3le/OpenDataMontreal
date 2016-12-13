7d4b49e910a8d1448eb7d0f3c83f777065459dcb
z3le <riordanova@gmail.com>
2016-12-13 19:19:36 +0200

    Change in CSV parser

---
+++ OpenDataMontreal/OpenDataMontreal.Parser/CSVParser/CsvFileParser.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using TinyCsvParser;
+using TinyCsvParser.Mapping;
+
+namespace OpenDataMontreal.Parser.CSVParser
+{
+    public class CsvFileParser<T> where T : class, new()
+    {
+        private CsvParserOptions _csvParserOptions;
+        private CsvReaderOptions _csvReaderOptions;
+
+        public CsvFileParser(char fieldsSeparator)
+        {
+            _csvParserOptions = new CsvParserOptions(true, new[] { fieldsSeparator });
+            _csvReaderOptions = new CsvReaderOptions(new[] { Environment.NewLine });
+        }
+
+        public List<CsvMappingResult<T>> ParseCsvString(string csvFile, CsvMapping<T> mapper)
+        {
+            var csvParser = new CsvParser<T>(_csvParserOptions, mapper);
+
+            var result = csvParser
+                .ReadFromString(_csvReaderOptions, csvFile)
+                .ToList();
+
+            return result;
+        }
+    }
+}
---
+++ OpenDataMontreal/src/OpenDataMontreal/Controllers/CrimeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OpenDataMontreal.Controllers
+{
+    public class CrimeController : Controller
+    {
+        // GET: /<controller>/
+        [Route("/[controller]")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
